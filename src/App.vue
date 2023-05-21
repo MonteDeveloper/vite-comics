b6a39f3d883f3e2c2f4aaac0aad83605d1a1e2ff
MonteDeveloper <lorenzomontello83@gmail.com>
2023-05-21 20:34:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import LogoNavbar from './components/LogoNavbar.vue'
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <LogoNavbar 
+      logoPath="./src/assets/img/dc-logo.png" 
+      navElements="CHARACTERS, COMICS, MOVIES, TV, GAMES, COLLECTIBLES, VIDEOS, FANS, NEWS, SHOP" 
+      />
+    </header>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/LogoNavbar.vue
@@ -0,0 +1,52 @@
+<script>
+
+export default {
+    name: "LogoNavbar",
+    props: {
+        logoPath: String,
+        navElements: String
+    },
+    data() {
+        return {
+            activeNavEl: 0
+        }
+    },
+    mounted() {
+        console.log("Caricato il componente 'LogoNavbar'.");
+    }
+}
+</script>
+
+<template>
+    <div class="d-flex justify-content-between align-items-center">
+        <img :src="logoPath" alt="LOGO">
+        <ul class="d-flex gap-4 p-0 m-0">
+            <li class="d-flex align-items-center" :class="index == activeNavEl ? 'activeNavEl' : ''" v-for="(li, index) in navElements.split(',')" @click="activeNavEl = index">
+                {{ li.trim() }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+img{
+    height: 100px;
+}
+
+li{
+    list-style: none;
+    height: 130px;
+    font-weight: 500;
+    transition: color .2s;
+}
+
+li:hover{
+    color: #0282f9;
+    cursor: pointer;
+}
+
+.activeNavEl{
+    color: #0282f9;
+    border-bottom: 4px solid #0282f9;
+}
+</style>
